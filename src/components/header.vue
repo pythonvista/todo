9d9827536aa80b2eff75607364080fe50e198dc0
Your Namepythonvista <you@example.comnablintech@gmail.com>
2022-11-11 14:25:11 +0100

    all this file

---
+++ src/components/header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">kbfokdsbhkfns/fjolsh'fs/dfjoh/fidsp</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.header{
+    height: 10vh;
+    background: red;
+}
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <headerVue/>
+  </div>
+</template>
+
+<script>
+import headerVue from '@/components/header.vue';
+export default {
+    components:{
+        headerVue
+    }
+
+}
+</script>
+
+<style>
+
+</style>
